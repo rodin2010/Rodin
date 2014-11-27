a33f9b98c587674c86f31b0389130405ddac2209
rodin2010 <rodin2010@gmail.com>
2014-11-27 20:57:04 +0800

    TestFrist

--- src/TestFrist.java
+++ src/TestFrist.java
@@ -3,7 +3,7 @@ public class TestFrist {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.err.println("sdfdsf");
+		System.err.println("sdfdsddf");
 
 	}
 
